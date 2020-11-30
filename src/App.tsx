b049c0813b0a19bbf2f6123808feb0b3b7745b79
Martin Omacht <enkeymc@gmail.com>
2020-11-30 12:17:29 +0100

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import TileEditor from "./pages/TileEditor";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/">
-
+            <TileEditor />
         </Route>
       </Switch>
     </Router>
---
+++ src/pages/TileEditor/index.tsx
@@ -0,0 +1,16 @@
+import DefaultLayout from "layouts/DefaultLayout";
+import React from "react";
+
+interface Props {
+
+}
+
+const TileEditor = (props: Props) => {
+    return (
+        <DefaultLayout>
+            <h1>Ahoj</h1>
+        </DefaultLayout>
+    );
+}
+
+export default TileEditor;
