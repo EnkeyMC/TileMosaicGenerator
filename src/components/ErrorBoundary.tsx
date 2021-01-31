00d8552f8fa2e4e5cb4e5bc3d72f4f2490d79667
Martin Omacht <enkeymc@gmail.com>
2021-01-31 15:44:54 +0100

    Fixed ErrorBoundary returning undefined

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -38,6 +38,7 @@ export default class ErrorBoundary extends React.Component<Props, State> {
                 </div>
             )
         }
-        return this.props.children;
+
+        return this.props.children ?? null;
     }
 }
